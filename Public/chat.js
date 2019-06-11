261be94c68275ca95cd11b7a274d08364ad2cf55
akshaymishra <akshaymishranishu@gmail.com>
2019-06-12 01:46:03 +0530

    socket connection made

---
+++ Public/chat.js
@@ -0,0 +1,2 @@
+//make socket connection with server
+var socket = io.connect('http://localhost:4000');
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var socket = require('socket.io');
 
 //App setup
 var app = express();
 
-app.listen(4000, function(){
+var server = app.listen(4000, function(){
     console.log('listening to port: 4000');
 });
 
 //use static
 app.use(express.static('public'));
+
+//socket setuo
+var io = socket(server);
+
+io.on('connection',function(socket){
+    console.log('socket connection made', socket.id);
+});
