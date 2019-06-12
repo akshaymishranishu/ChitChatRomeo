38b8e1fcd78482b75baa6b294fa238ff3f0813e4
akshaymishra <akshaymishranishu@gmail.com>
2019-06-13 01:02:50 +0530

    Revert "added a broadcasting"
    This reverts commit 8c25147c2f708fcab039e506b55e3b4b53e0215e.

--- Public/chat.js
+++ Public/chat.js
@@ -1,5 +1,3 @@
-//chat.js is node file on the front-end
-
 //make socket connection with server
 var socket = io.connect('http://localhost:4000');
 
@@ -8,32 +6,16 @@ var message = document.getElementById('message');
 handle = document.getElementById('handle');
 btn = document.getElementById('send');
 output = document.getElementById('output');
-feedback = document.getElementById('feedback');
 
-//listen button click event when client press the send button
+//Emit events
 btn.addEventListener('click', function(){
-
-    //emits the data to the server
     socket.emit('chat', {
         message: message.value,
         handle: handle.value
     });
 });
 
-//listen for messagetyping event
-message.addEventListener('keypress', function(){
-    
-    //emits the data to the server
-    socket.emit('typing', handle.value);
-});
-
-//Listen for events emitted by server
+//Listen for events
 socket.on('chat', function(data){
-    feedback.innerHTML = "";  //after pressing the send button the message(xyz.. is typing message) will get vanished
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
-});
-
-//Listen for events emitted by server
-socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>' + data + 'is typing a message...</em></p>'
 });
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-//index.js is a node file on back-end
-
 var express = require('express');
 var socket = require('socket.io');
 
 //App setup
 var app = express();
 
-//created a server which is listening at port: 4000 on localhost
 var server = app.listen(4000, function(){
     console.log('listening to port: 4000');
 });
@@ -17,21 +14,13 @@ app.use(express.static('public'));
 //socket setup
 var io = socket(server);
 
-//listen to the cient to make a socket persistant connection with the client
 io.on('connection',function(socket){
     console.log('socket connection made', socket.id);
 
     //Listen for chat event to be emitted by cilent (chat.js) and take the data
     socket.on('chat', function(data){
 
-        //it emit to all the clients (including to the sender) connected with this socket
+        //it emit the event to all the clients connected with this socket
         io.emit('chat',data);
-    });
-
-    //Listen for typing event by the client
-    socket.on('typing', function(data){
-
-        //it emit to all the clients (nut not to the sender) connected with this socket
-        socket.broadcast.emit('typing',data);
     });
 });
